361c7de558b6eb46f728bd8182e7d96f0a43645e
JuliusR <>
2019-06-16 09:04:04 +0200

    Add refresh button

--- index.php
+++ index.php
@@ -9,34 +9,69 @@ require_once("parse.php");
 require_once("format_bnn.php");
 require_once("format_csv.php");
 
-$missing_producer_keys = [];
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>HTML2Sharedlists</title>
+    <style type="text/css">input { font-size: 500%; }</style>
+  </head>
+  <body>
+    <form method="post">
+      <fieldset>
+	<input type="hidden" name="refresh" value="refresh" />
+	<input type="submit" value="Refresh" onclick="this.disabled = true; this.value='Refreshing...';" />
+      </fieldset>
+    </form>
+<?php
 
-foreach($kind_sources as $kind => $sources) {
-    $articles = array();
-    foreach($sources as $src) {
-        // fetch
-        $html = fetch_html($src);
+if(isset($_POST['refresh']) || isset($_GET['always_refresh'])) {
+    echo '<pre>';
 
-        // parse
-        $articles = array_merge(
-            $articles,
-            parse_articles($html)
-        );
+    $missing_producer_keys = [];
+    $article_write_counts = [];
+
+    foreach($kind_sources as $kind => $sources) {
+        $articles = array();
+        foreach($sources as $src) {
+            // fetch
+            $html = fetch_html($src);
+
+            // parse
+            $articles = array_merge(
+                $articles,
+                parse_articles($html)
+            );
+        }
+
+        // format
+        $csv = format_csv($articles);
+        file_put_contents('out/' . $kind . '.csv', $csv);
+
+        // format
+        $bnn = format_bnn($articles);
+        $bnn = mb_convert_encoding($bnn, 'CP850', 'UTF-8');
+        file_put_contents('out/PL' . $kind . '.BNN', $bnn);
+
+        $article_write_counts[$kind] = count($articles);
+
+        if(count($missing_producer_keys) > 0) {
+            echo "WARNING: possibly missing producer keys.<br />\n";
+            var_dump($missing_producer_keys);
+            echo "<br />\n";
+            $missing_producer_keys = [];
+        }
     }
+    echo '</pre>';
 
-    // format
-    $csv = format_csv($articles);
-    file_put_contents('out/' . $kind . '.csv', $csv);
-
-    // format
-    $bnn = format_bnn($articles);
-    $bnn = mb_convert_encoding($bnn, 'CP850', 'UTF-8');
-    file_put_contents('out/PL' . $kind . '.BNN', $bnn);
-
-    if(count($missing_producer_keys) > 0) {
-        echo "WARNING: possibly missing producer keys.<br />\n";
-        var_dump($missing_producer_keys);
-        echo "<br />\n";
-        $missing_producer_keys = [];
+    echo '<h1>Refresh succeeded for:</h1>';
+    echo '<dl>';
+    foreach($article_write_counts as $kind => $count) {
+        echo "<dt>$kind</dt><dd>$count articles</dd>";
     }
+    echo '</dl>';
 }
+?>
+  </body>
+</html>
